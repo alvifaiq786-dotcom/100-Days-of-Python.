e6154f6beef349685939a5db7e2a0a80695da2ba
DARK REAPER <alvifaiq786@gmail.com>
2025-11-02 15:34:33 +0530

    Add files via upload

---
+++ newprogram.py
@@ -0,0 +1,22 @@
+a = int(input("enter your age: "))
+
+# if statement no: 1
+if(a%2 == 0):
+    print("a is even")
+# end of the statement no: 1 
+
+# if statement no: 2
+if(a>=18):
+    print("you are above the age of consent")
+    print("good for you ")
+
+elif(a<0):
+    print("you are entering an invalid negative age ")
+
+else:
+    print("you are the below thr age of consent")
+
+#end of the if statement no: 2
+
+print("end of program ")
+
---
+++ pratice.py
@@ -0,0 +1,98 @@
+# # # a1 = int(input("enter number 1: "))
+# # # a2 = int(input("enter number 2: "))
+# # # a3 = int(input("enter number 3: "))
+# # # a4 = int(input("enter number 4: "))
+
+# # # if(a1>a2 and a1>a3 and a1>a4):
+# # #     print("greatest number is a1:", a1)
+
+# # # elif(a2>a1 and a2>a3 and a2>a4):
+# # #     print("greatest number is a2:", a2)
+
+# # # elif(a3>a1 and a3>a2 and a3>a4):
+# # #     print("greatest number is a3:", a3)
+
+# # # elif(a4>a1 and a4>a2 and a4>a3):
+# # #     print("greatest number is a4:", a4)
+
+
+# # marks1 = int(input("enter the marks1: "))
+# # marks2 = int(input("enter the marks2: "))
+# # marks3 = int(input("enter the marks3: "))
+
+# # total_percentage = (100)*(marks1 + marks2 + marks3)/300
+
+# # if(total_percentage>=40 and marks1>=33 and marks2>=33 and marks3>=33):
+# #     print("you are pass", total_percentage)
+
+# # else:
+# #     print("you are fail try again next year!",total_percentage)
+
+# # p1 = "make a lot of money" 
+# # p2 = "buy now"
+# # p3 = "sunscibe this"
+# # p4 = "click this"
+
+
+# # message = input("enter your comment: ")
+
+# # if((p1 in message) or (p2 in message) or (p3 in message) or (p4 in message)):
+# #     print("this is comment in spam ")
+
+# # else:
+# #     print("this comment is not a spam")    
+
+
+
+# # username = input("enter username")
+
+# # if(len(username)<10):
+# #     print("your username contain less than 10 characters")
+# # else:
+# #     print("all is well")
+
+
+
+
+# l = ["harry", "rohan", "vipul", "asra", "xara" ]
+
+# name = input("enter your name: ")
+  
+# if(name in l):
+#     print("your name is in the list")
+
+# else:
+#      print("your name is not in the list")
+
+# marks = int(input("enter your marks: "))
+
+# if(marks<=100 and marks >=90):
+#     grade = "EX"
+
+# elif(marks<90 and marks >=80):
+#     grade = "A"
+# elif(marks<80 and marks >=70):
+#     grade = "B"
+# elif(marks<70 and marks >=60):
+#     grade = "C"
+# elif(marks<60 and marks >=50):
+#     grade = "D"
+# elif(marks<50):
+#     grade = "E"
+
+# print("your grade is:", grade)
+
+    
+
+# post = input("enter the post: ")
+
+# if("harry".lower() in post.lower()):
+#     print("this post is talking about harry ")
+
+# else:
+#     print("this post is not talking about harry")    
+
+
+
+# for i in range(1,100):
+#     print(i)
